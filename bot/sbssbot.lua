cb612f7c0cccb596c2d42b499b81b1b3f170c1cf
MSAteam <MSAteam0@gmail.com>
2016-09-23 03:04:08 +0330

    Update sbssbot.lua

--- bot/sbssbot.lua
+++ bot/sbssbot.lua
@@ -239,18 +239,18 @@ function create_config( )
     "time",
     "welcome"
     },
-    sudo_users = {144658773,114934518,94746365},--Sudo users
+    sudo_users = {243375481},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Sbss Bot V2 Beta
     An Advanced Anti Spam Bot Forked On TeleSeed
     
     Develpoed By:
-    @sina7sk
+    @msa2savan
     Manager:
-    @yellowhat
+    @MSAteamBOTadmin
     Founder:
-    @amirho3in
+    @Oo_MSA_oO
     
     Special Thank To:
     Mehr Pouya
